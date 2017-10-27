3474be2f8c7d4f772140608588c757c8fca2915a
jwwangchn <jwwangchn@outlook.com>
2017-10-27 22:40:40 +0800

    add HOG+SVM train test code

---
+++ HOG/digitals_read_data.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+
+import cv2
+import numpy as np
+from common import clock, mosaic
+
+SZ = 20
+CLASS_N = 10
+
+
+# local modules
+
+def split2d(img, cell_size, flatten=True):
+    h, w = img.shape[:2]
+    sx, sy = cell_size
+    cells = [np.hsplit(row, w // sx) for row in np.vsplit(img, h // sy)]
+    cells = np.array(cells)
+    if flatten:
+        cells = cells.reshape(-1, sy, sx)
+    return cells
+
+
+def load_digits(fn):
+    digits_img = cv2.imread(fn, 0)
+    digits = split2d(digits_img, (SZ, SZ))
+    labels = np.repeat(np.arange(CLASS_N), len(digits) / CLASS_N)
+    return digits, labels
+
+
+def deskew(img):
+    m = cv2.moments(img)
+    if abs(m['mu02']) < 1e-2:
+        return img.copy()
+    skew = m['mu11'] / m['mu02']
+    M = np.float32([[1, skew, -0.5 * SZ * skew], [0, 1, 0]])
+    img = cv2.warpAffine(img, M, (SZ, SZ), flags=cv2.WARP_INVERSE_MAP | cv2.INTER_LINEAR)
+    return img
